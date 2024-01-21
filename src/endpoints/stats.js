30fe4fff1d680442ae0e7f2c82b08af00cc152dd
anastasialukanova <anastasia.lukanova@student.nhlstenden.com>
2024-01-21 23:40:47 +0100

    most/least liked movies

--- src/endpoints/stats.js
+++ src/endpoints/stats.js
@@ -46,4 +46,46 @@ router.get('/leastViewed', auth_middleware, async (req, res) => {
     }
 });
 
+// most liked
+router.get('/mostLiked', auth_middleware, async (req, res) => {
+    try {
+        let { year, month } = req.query;
+
+        if (!year || !month || isNaN(year) || isNaN(month)) {
+            res.status(400).json({ error: 'Invalid request parameters' });
+            return;
+        }
+
+        let stats = await sql`
+            SELECT * FROM public.get_sorted_movies_most_liked(${year}, ${month})
+        `;
+        
+        res.status(200).json(stats);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// least liked
+router.get('/leastLiked', auth_middleware, async (req, res) => {
+    try {
+        let { year, month } = req.query;
+
+        if (!year || !month || isNaN(year) || isNaN(month)) {
+            res.status(400).json({ error: 'Invalid request parameters' });
+            return;
+        }
+
+        let stats = await sql`
+            SELECT * FROM public.get_sorted_movies_least_liked(${year}, ${month})
+        `;
+        
+        res.status(200).json(stats);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 export default router;
