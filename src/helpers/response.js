b5700c2e0adc854c19c98dade43e6dd9e36d916c
paul <112290055+2560p@users.noreply.github.com>
2024-03-09 13:40:25 +0100

    fix: stable xml parser

--- src/helpers/response.js
+++ src/helpers/response.js
@@ -1,12 +1,17 @@
-const js2xmlparser = require('js2xmlparser');
+const { js2xml } = require('xml-js');
 
-function jsonToXml(jsonData, rootElement = 'root') {
-    return js2xmlparser.parse(rootElement, jsonData);
+function jsonToXml(jsonData) {
+    return js2xml(jsonData, {
+        compact: true,
+        ignoreComment: true,
+        spaces: 4,
+    });
 }
 
-function respond(req, res, data, status = 200) {
+function respond(req, res, data, xml_root = null, status = 200) {
     if (req.headers.accept === 'application/xml') {
         res.set('Content-Type', 'application/xml');
+        data = xml_root ? { [xml_root]: data } : data;
         res.status(status).send(jsonToXml(data));
         return;
     }
