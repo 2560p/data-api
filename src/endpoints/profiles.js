06e22e848f73c8bfa52bc3442fac9b44391be8c2
paul <112290055+2560p@users.noreply.github.com>
2024-03-10 14:50:45 +0100

    add: put for currently_watched

--- src/endpoints/profiles.js
+++ src/endpoints/profiles.js
@@ -182,26 +182,140 @@ router.delete('/:id', auth_middleware, require_admin, async (req, res) => {
 // currently_watched requests
 
 // get request
-router.get('/:id/currently_watched', auth_middleware, async (req, res) => {
-    let id = parseInt(req.params.id);
+router.get(
+    '/:id/currently_watched',
+    auth_middleware,
+    require_admin,
+    async (req, res) => {
+        let id = parseInt(req.params.id);
 
-    if (!Number.isInteger(id)) {
-        respond(req, res, { error: 'Invalid id' }, null, 400);
-        return;
-    }
+        if (!Number.isInteger(id)) {
+            respond(req, res, { error: 'Invalid id' }, null, 400);
+            return;
+        }
 
-    let currently_watched = await sql`select 
+        // check if profile exists
+        let profile_check = await sql`select 1 from profiles where id = ${id}`;
+        if (profile_check.length === 0) {
+            respond(req, res, { error: 'Profile not found' }, null, 404);
+            return;
+        }
+
+        let currently_watched = await sql`select 
                     media_id, progress_seconds, last_watched 
                     from currently_watched
                     where profile_id = ${id}
                     `;
 
-    if (currently_watched.length === 0) {
-        respond(req, res, { error: 'No currently_watched found' }, null, 404);
-        return;
-    }
+        if (currently_watched.length === 0) {
+            respond(
+                req,
+                res,
+                { error: 'No currently watched media found' },
+                null,
+                404
+            );
+            return;
+        }
 
-    respond(req, res, currently_watched, 'media');
-});
+        respond(req, res, currently_watched, 'currently_watched_media');
+    }
+);
+
+// put request
+router.put(
+    '/:id/currently_watched/:media_id',
+    auth_middleware,
+    require_admin,
+    async (req, res) => {
+        let profile_id = parseInt(req.params.id);
+        let media_id = parseInt(req.params.media_id);
+        let body = req.body;
+
+        if (!Number.isInteger(profile_id) || !Number.isInteger(media_id)) {
+            respond(req, res, { error: 'Invalid id' }, null, 400);
+            return;
+        }
+
+        if (!body || !body.progress_seconds) {
+            respond(req, res, { error: 'Invalid request' }, null, 400);
+            return;
+        }
+
+        let { progress_seconds, last_watched } = body;
+
+        // if no last_watched is provided, use the current datetime
+        if (last_watched !== undefined) {
+            // last_watched should be a valid datetime in format YYYY-MM-DD HH:MM:SS
+            let last_watched_regex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/;
+
+            if (
+                !last_watched.match(last_watched_regex) ||
+                isNaN(Date.parse(last_watched))
+            ) {
+                respond(req, res, { error: 'Invalid request' }, null, 400);
+                return;
+            }
+        } else {
+            // this also has to follow the datetime regex
+            last_watched = new Date()
+                .toISOString()
+                .slice(0, 19)
+                .replace('T', ' ');
+        }
+
+        // progress_seconds should be a positive integer
+        if (!Number.isInteger(progress_seconds) || progress_seconds < 0) {
+            respond(req, res, { error: 'Invalid request' }, null, 400);
+            return;
+        }
+
+        let profile_check =
+            await sql`select 1 from profiles where id = ${profile_id}`;
+        if (profile_check.length === 0) {
+            respond(req, res, { error: 'Profile not found' }, null, 404);
+            return;
+        }
+
+        let media_check = await sql`select 1 from media where id = ${media_id}`;
+        if (media_check.length === 0) {
+            respond(req, res, { error: 'Media not found' }, null, 404);
+            return;
+        }
+
+        // check if media is already being watched
+        let currently_watched_check =
+            await sql`select 1 from currently_watched where profile_id = ${profile_id} and media_id = ${media_id}`;
+
+        if (currently_watched_check.length === 0) {
+            await sql`
+                insert into currently_watched (profile_id, media_id, progress_seconds, last_watched)
+                values (${profile_id}, ${media_id}, ${progress_seconds}, ${last_watched})
+            `;
+            respond(
+                req,
+                res,
+                { message: 'Currently watched media added' },
+                'success',
+                201
+            );
+            return;
+        }
+
+        await sql`
+            update currently_watched
+            set progress_seconds = ${progress_seconds}, last_watched = ${last_watched}
+            where profile_id = ${profile_id} and media_id = ${media_id}
+            returning 1
+        `;
+
+        respond(
+            req,
+            res,
+            { message: 'Currently watched media updated successfully' },
+            'success'
+        );
+    }
+);
 
 export default router;
